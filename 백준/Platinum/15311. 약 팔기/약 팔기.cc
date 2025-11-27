e426d479a6ba5e765dd597539a156e6eb0b8d466
alsrhsk <minguan1537@gmail.com>
2025-11-27 10:27:23 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+{
+    int i;
+    printf("1999\n");
+    for(i=1; i<=1000; i++)
+    {
+        printf("1 ");
+    }
+    for(i=1; i<=999; i++)
+    {
+        printf("1000 ");
+    }
+}
